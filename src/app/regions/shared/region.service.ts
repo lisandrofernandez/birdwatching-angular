166f0222d80bf56f1a643e1741e90c6dee46ef0d
Lisandro Fernandez <kelechul@gmail.com>
2020-07-21 16:24:01 +0200

    Make RegionService extends ServiceErrorHandler

--- src/app/regions/shared/region.service.ts
+++ src/app/regions/shared/region.service.ts
@@ -4,22 +4,21 @@ import { HttpClient } from '@angular/common/http';
 import { Observable, of } from 'rxjs';
 import { catchError } from 'rxjs/operators';
 
+import { ServiceErrorHandler } from '../../common/service-error-handler';
 import { Region } from './region.model';
 
 @Injectable({ providedIn: 'root' })
-export class RegionService {
+export class RegionService extends ServiceErrorHandler {
   private regionsUrl = 'http://localhost:8080/api/v1/regions';
 
-  constructor(private http: HttpClient) { }
+  constructor(private http: HttpClient) {
+    super();
+  }
 
   /** GET regions from the server */
   getRegions(): Observable<Region[]> {
     return this.http.get<Region[]>(this.regionsUrl).pipe(
-      catchError(error => {
-        // TODO: show a message with the error
-        console.error(error);
-        return of([]);
-      })
+      catchError(this.handleError)
     );
   }
 
