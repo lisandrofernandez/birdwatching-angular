34b78cba19032e90c77eaa2da1cc97c488504356
Lisandro Fernandez <kelechul@gmail.com>
2020-07-22 11:48:01 +0200

    Add regions cache in RegionService

--- src/app/regions/shared/region.service.ts
+++ src/app/regions/shared/region.service.ts
@@ -1,8 +1,8 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 
-import { Observable, of } from 'rxjs';
-import { catchError } from 'rxjs/operators';
+import { Observable } from 'rxjs';
+import { catchError, shareReplay } from 'rxjs/operators';
 
 import { ServiceErrorHandler } from '../../common/service-error-handler';
 import { Region } from './region.model';
@@ -10,16 +10,25 @@ import { Region } from './region.model';
 @Injectable({ providedIn: 'root' })
 export class RegionService extends ServiceErrorHandler {
   private regionsUrl = 'http://localhost:8080/api/v1/regions';
+  private regionsCache$: Observable<Region[]>;
 
   constructor(private http: HttpClient) {
     super();
   }
 
-  /** GET regions from the server */
+  /**
+   * GET regions from the server
+   *
+   * Caches the response for further use.
+   */
   getRegions(): Observable<Region[]> {
-    return this.http.get<Region[]>(this.regionsUrl).pipe(
-      catchError(this.handleError)
-    );
+    if (!this.regionsCache$) {
+      this.regionsCache$ = this.http.get<Region[]>(this.regionsUrl).pipe(
+        shareReplay(1),
+        catchError(this.handleError)
+      );
+    }
+    return this.regionsCache$;
   }
 
 }
